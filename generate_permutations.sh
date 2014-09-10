3f2c0ade459fb8839122f2fc7f9435054c197466
andek <andek034@gmail.com>
2014-09-10 15:39:15 +0200

    Added permutations for Beijing study

--- generate_permutations.sh
+++ generate_permutations.sh
@@ -2,13 +2,19 @@
 
 clear
 
+#Study=Cambridge
+#Filename=Cambridge_Buckner
+
+Study=Beijing
+Filename=Beijing_Zang
+
 # Loop over many random group comparisons
 for Comparison in {1..1000}
 do
 	echo "Making random permutation $Comparison !"
 
 	# Make a random permutation of all subjects
-	shuf /home/andek/Data/fcon1000/Cambridge/Cambridge_Buckner_subjects.txt > /home/andek/Research_projects/RandomGroupAnalyses/Cambridge_permutations/permutation${Comparison}.txt
+	shuf /home/andek/Data/fcon1000/${Study}/${Filename}_subjects.txt > /home/andek/Research_projects/RandomGroupAnalyses/${Study}_permutations/permutation${Comparison}.txt
 
 done
 
