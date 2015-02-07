cdc96a3018f106d5abe633adfa86fdf5cac805ad
Anders Eklund <andek@triton.(none)>
2015-02-07 11:08:48 +0100

    Updated one sample t-test permutation scripts

--- FSL/run_random_group_analyses_onesamplettest_groupsize40_nonparametric.sh
+++ FSL/run_random_group_analyses_onesamplettest_groupsize40_nonparametric.sh
@@ -79,6 +79,26 @@ do
 				Subject18=${Subjects[$((17))]}
 				Subject19=${Subjects[$((18))]}
 				Subject20=${Subjects[$((19))]}
+				Subject21=${Subjects[$((20))]}
+				Subject22=${Subjects[$((21))]}
+				Subject23=${Subjects[$((22))]}
+				Subject24=${Subjects[$((23))]}
+				Subject25=${Subjects[$((24))]}
+				Subject26=${Subjects[$((25))]}
+				Subject27=${Subjects[$((26))]}
+				Subject28=${Subjects[$((27))]}
+				Subject29=${Subjects[$((28))]}
+				Subject30=${Subjects[$((29))]}
+				Subject31=${Subjects[$((30))]}
+				Subject32=${Subjects[$((31))]}
+				Subject33=${Subjects[$((32))]}
+				Subject34=${Subjects[$((33))]}
+				Subject35=${Subjects[$((34))]}
+				Subject36=${Subjects[$((35))]}
+				Subject37=${Subjects[$((36))]}
+				Subject38=${Subjects[$((37))]}
+				Subject39=${Subjects[$((38))]}
+				Subject40=${Subjects[$((39))]}
 
 				#-------------------------------------
 				# Put data into one nifti file
@@ -106,13 +126,33 @@ do
 				Results/${Study}/${Smoothing}/${Design}/${Subject17}.feat/reg_standard/stats/cope1.nii.gz \
 				Results/${Study}/${Smoothing}/${Design}/${Subject18}.feat/reg_standard/stats/cope1.nii.gz \
 				Results/${Study}/${Smoothing}/${Design}/${Subject19}.feat/reg_standard/stats/cope1.nii.gz \
-				Results/${Study}/${Smoothing}/${Design}/${Subject20}.feat/reg_standard/stats/cope1.nii.gz 
+				Results/${Study}/${Smoothing}/${Design}/${Subject20}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject21}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject22}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject23}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject24}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject25}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject26}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject27}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject28}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject29}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject30}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject31}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject32}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject33}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject34}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject35}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject36}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject37}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject38}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject39}.feat/reg_standard/stats/cope1.nii.gz \
+				Results/${Study}/${Smoothing}/${Design}/${Subject40}.feat/reg_standard/stats/cope1.nii.gz 
 
 				#-------------------------------------------------------
 				# Run group analysis using permutation test in BROCCOLI
 				#-------------------------------------------------------
 
-				./RandomiseGroupLevel all_subjects.nii.gz -groupmean -mask MNI152_T1_2mm_brain_mask.nii.gz -permutations 1000 -quiet -cdt ${ClusterDefiningThreshold} -permutationfile permutations_onesamplettest_groupsize20.txt > log.txt
+				./RandomiseGroupLevel all_subjects.nii.gz -groupmean -mask MNI152_T1_2mm_brain_mask.nii.gz -permutations 1000 -quiet -cdt ${ClusterDefiningThreshold} -permutationfile permutations_onesamplettest_groupsize40.txt > log.txt
 
 				# Equivalent call to randomise
 				# randomise -i all_subjects.nii.gz -o test -1 -P -c ${ClusterDefiningThreshold} -n 1001 -m MNI152_T1_2mm_brain_mask.nii.gz
@@ -128,7 +168,7 @@ do
 	
 			done
 
-			echo "Out of $Comparison random group analyses, significant group activations were detected $SignificantActivations times !" > /home/andek/Research_projects/RandomGroupAnalyses/Results/results_broccoli_onesamplettest_${Study}_${Design}_${Smoothing}_perm_${ClusterDefiningThreshold}_groupsize20.txt
+			echo "Out of $Comparison random group analyses, significant group activations were detected $SignificantActivations times !" > /home/andek/Research_projects/RandomGroupAnalyses/Results/results_broccoli_onesamplettest_${Study}_${Design}_${Smoothing}_perm_${ClusterDefiningThreshold}_groupsize40.txt
 
 		done
 	done
