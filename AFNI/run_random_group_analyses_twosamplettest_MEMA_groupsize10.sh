630bb686aedea7a7a5a9ac8952e687032c197879
Anders Eklund <andek@Laverda.imt.liu.se>
2015-03-30 14:26:28 +0200

    Added MEMA scripts

--- AFNI/run_random_group_analyses_twosamplettest_MEMA_groupsize10.sh
+++ AFNI/run_random_group_analyses_twosamplettest_MEMA_groupsize10.sh
@@ -7,15 +7,15 @@ clear
 #Study=Cambridge
 Study=Beijing
 
-thirtynine=39
-NumberOfSubjects=40.0
+nineteen=19
+NumberOfSubjects=20.0
 
 ClusterDefiningThresholdP1=0.01
-ClusterDefiningThreshold1=2.429
+ClusterDefiningThreshold1=2.552
 CDT1=2.3
 
 ClusterDefiningThresholdP2=0.001
-ClusterDefiningThreshold2=3.319
+ClusterDefiningThreshold2=3.610
 CDT2=3.1	
 
 for D in 1 2 3 4
@@ -107,27 +107,6 @@ do
 			Subject18=${Subjects[$((17))]}
 			Subject19=${Subjects[$((18))]}
 			Subject20=${Subjects[$((19))]}
-	
-			Subject21=${Subjects[$((20))]}
-			Subject22=${Subjects[$((21))]}
-			Subject23=${Subjects[$((22))]}
-			Subject24=${Subjects[$((23))]}
-			Subject25=${Subjects[$((24))]}
-			Subject26=${Subjects[$((25))]}
-			Subject27=${Subjects[$((26))]}
-			Subject28=${Subjects[$((27))]}
-			Subject29=${Subjects[$((28))]}
-			Subject30=${Subjects[$((29))]}
-			Subject31=${Subjects[$((30))]}
-			Subject32=${Subjects[$((31))]}
-			Subject33=${Subjects[$((32))]}
-			Subject34=${Subjects[$((33))]}
-			Subject35=${Subjects[$((34))]}
-			Subject36=${Subjects[$((35))]}
-			Subject37=${Subjects[$((36))]}
-			Subject38=${Subjects[$((37))]}
-			Subject39=${Subjects[$((38))]}
-			Subject40=${Subjects[$((39))]}
 
 			#echo "$GroupDirectory/${Subject1}.results/stats.${Subject1}+tlrc[0]"
 	
@@ -152,7 +131,7 @@ do
 		            $GroupDirectory/${Subject8}.results/full_mask.${Subject8}+tlrc.HEAD \
 		            $GroupDirectory/${Subject9}.results/full_mask.${Subject9}+tlrc.HEAD \
 		            $GroupDirectory/${Subject10}.results/full_mask.${Subject10}+tlrc.HEAD \
-	    	       	    $GroupDirectory/${Subject11}.results/full_mask.${Subject11}+tlrc.HEAD \
+	    	        $GroupDirectory/${Subject11}.results/full_mask.${Subject11}+tlrc.HEAD \
 		            $GroupDirectory/${Subject12}.results/full_mask.${Subject12}+tlrc.HEAD \
 		            $GroupDirectory/${Subject13}.results/full_mask.${Subject13}+tlrc.HEAD \
 		            $GroupDirectory/${Subject14}.results/full_mask.${Subject14}+tlrc.HEAD \
@@ -161,27 +140,7 @@ do
 		            $GroupDirectory/${Subject17}.results/full_mask.${Subject17}+tlrc.HEAD \
 		            $GroupDirectory/${Subject18}.results/full_mask.${Subject18}+tlrc.HEAD \
 		            $GroupDirectory/${Subject19}.results/full_mask.${Subject19}+tlrc.HEAD \
-		            $GroupDirectory/${Subject20}.results/full_mask.${Subject20}+tlrc.HEAD \
-		            $GroupDirectory/${Subject21}.results/full_mask.${Subject21}+tlrc.HEAD \
-		            $GroupDirectory/${Subject22}.results/full_mask.${Subject22}+tlrc.HEAD \
-		            $GroupDirectory/${Subject23}.results/full_mask.${Subject23}+tlrc.HEAD \
-		            $GroupDirectory/${Subject24}.results/full_mask.${Subject24}+tlrc.HEAD \
-		            $GroupDirectory/${Subject25}.results/full_mask.${Subject25}+tlrc.HEAD \
-		            $GroupDirectory/${Subject26}.results/full_mask.${Subject26}+tlrc.HEAD \
-		            $GroupDirectory/${Subject27}.results/full_mask.${Subject27}+tlrc.HEAD \
-		            $GroupDirectory/${Subject28}.results/full_mask.${Subject28}+tlrc.HEAD \
-		            $GroupDirectory/${Subject29}.results/full_mask.${Subject29}+tlrc.HEAD \
-		            $GroupDirectory/${Subject30}.results/full_mask.${Subject30}+tlrc.HEAD \
-		            $GroupDirectory/${Subject31}.results/full_mask.${Subject31}+tlrc.HEAD \
-		            $GroupDirectory/${Subject32}.results/full_mask.${Subject32}+tlrc.HEAD \
-		            $GroupDirectory/${Subject33}.results/full_mask.${Subject33}+tlrc.HEAD \
-		            $GroupDirectory/${Subject34}.results/full_mask.${Subject34}+tlrc.HEAD \
-		            $GroupDirectory/${Subject35}.results/full_mask.${Subject35}+tlrc.HEAD \
-		            $GroupDirectory/${Subject36}.results/full_mask.${Subject36}+tlrc.HEAD \
-		            $GroupDirectory/${Subject37}.results/full_mask.${Subject37}+tlrc.HEAD \
-		            $GroupDirectory/${Subject38}.results/full_mask.${Subject38}+tlrc.HEAD \
-		            $GroupDirectory/${Subject39}.results/full_mask.${Subject39}+tlrc.HEAD \
-		            $GroupDirectory/${Subject40}.results/full_mask.${Subject40}+tlrc.HEAD 
+		            $GroupDirectory/${Subject20}.results/full_mask.${Subject20}+tlrc.HEAD 
 
 			# Check if group mask was created correctly
 		    if [ -e $ResultsDirectory/group_mask+tlrc.HEAD  ]; then
@@ -200,6 +159,7 @@ do
 			             ${Subject8} "$GroupDirectory/${Subject8}.results/stats.${Subject8}_REML+tlrc[1]" "$GroupDirectory/${Subject8}.results/stats.${Subject8}_REML+tlrc[2]"\
 			             ${Subject9} "$GroupDirectory/${Subject9}.results/stats.${Subject9}_REML+tlrc[1]" "$GroupDirectory/${Subject9}.results/stats.${Subject9}_REML+tlrc[2]"\
 			             ${Subject10} "$GroupDirectory/${Subject10}.results/stats.${Subject10}_REML+tlrc[1]" "$GroupDirectory/${Subject10}.results/stats.${Subject10}_REML+tlrc[2]"\
+		    	      -set BB                                               \
 			             ${Subject11} "$GroupDirectory/${Subject11}.results/stats.${Subject11}_REML+tlrc[1]" "$GroupDirectory/${Subject11}.results/stats.${Subject11}_REML+tlrc[2]"\
 			             ${Subject12} "$GroupDirectory/${Subject12}.results/stats.${Subject12}_REML+tlrc[1]" "$GroupDirectory/${Subject12}.results/stats.${Subject12}_REML+tlrc[2]"\
 			             ${Subject13} "$GroupDirectory/${Subject13}.results/stats.${Subject13}_REML+tlrc[1]" "$GroupDirectory/${Subject13}.results/stats.${Subject13}_REML+tlrc[2]"\
@@ -208,29 +168,8 @@ do
 			             ${Subject16} "$GroupDirectory/${Subject16}.results/stats.${Subject16}_REML+tlrc[1]" "$GroupDirectory/${Subject16}.results/stats.${Subject16}_REML+tlrc[2]"\
 			             ${Subject17} "$GroupDirectory/${Subject17}.results/stats.${Subject17}_REML+tlrc[1]" "$GroupDirectory/${Subject17}.results/stats.${Subject17}_REML+tlrc[2]"\
 			             ${Subject18} "$GroupDirectory/${Subject18}.results/stats.${Subject18}_REML+tlrc[1]" "$GroupDirectory/${Subject18}.results/stats.${Subject18}_REML+tlrc[2]"\
-			             ${Subject19} "$GroupDirectory/${Subject19}.results/stats.${Subject19}_REML+tlrc[1]" "$GroupDirectory/${Subject19}.results/stats.${Subject19}_REML+tlrc[2]"\
-			             ${Subject20} "$GroupDirectory/${Subject20}.results/stats.${Subject20}_REML+tlrc[1]" "$GroupDirectory/${Subject20}.results/stats.${Subject20}_REML+tlrc[2]"\
-		    	      -set BB                                               \
-			             ${Subject21} "$GroupDirectory/${Subject21}.results/stats.${Subject21}_REML+tlrc[1]" "$GroupDirectory/${Subject21}.results/stats.${Subject21}_REML+tlrc[2]"\
-			             ${Subject22} "$GroupDirectory/${Subject22}.results/stats.${Subject22}_REML+tlrc[1]" "$GroupDirectory/${Subject22}.results/stats.${Subject22}_REML+tlrc[2]"\
-			             ${Subject23} "$GroupDirectory/${Subject23}.results/stats.${Subject23}_REML+tlrc[1]" "$GroupDirectory/${Subject23}.results/stats.${Subject23}_REML+tlrc[2]"\
-			             ${Subject24} "$GroupDirectory/${Subject24}.results/stats.${Subject24}_REML+tlrc[1]" "$GroupDirectory/${Subject24}.results/stats.${Subject24}_REML+tlrc[2]"\
-			             ${Subject25} "$GroupDirectory/${Subject25}.results/stats.${Subject25}_REML+tlrc[1]" "$GroupDirectory/${Subject25}.results/stats.${Subject25}_REML+tlrc[2]"\
-			             ${Subject26} "$GroupDirectory/${Subject26}.results/stats.${Subject26}_REML+tlrc[1]" "$GroupDirectory/${Subject26}.results/stats.${Subject26}_REML+tlrc[2]"\
-			             ${Subject27} "$GroupDirectory/${Subject27}.results/stats.${Subject27}_REML+tlrc[1]" "$GroupDirectory/${Subject27}.results/stats.${Subject27}_REML+tlrc[2]"\
-			             ${Subject28} "$GroupDirectory/${Subject28}.results/stats.${Subject28}_REML+tlrc[1]" "$GroupDirectory/${Subject28}.results/stats.${Subject28}_REML+tlrc[2]"\
-		    	             ${Subject29} "$GroupDirectory/${Subject29}.results/stats.${Subject29}_REML+tlrc[1]" "$GroupDirectory/${Subject29}.results/stats.${Subject29}_REML+tlrc[2]"\
-			             ${Subject30} "$GroupDirectory/${Subject30}.results/stats.${Subject30}_REML+tlrc[1]" "$GroupDirectory/${Subject30}.results/stats.${Subject30}_REML+tlrc[2]"\
-			             ${Subject31} "$GroupDirectory/${Subject31}.results/stats.${Subject31}_REML+tlrc[1]" "$GroupDirectory/${Subject31}.results/stats.${Subject31}_REML+tlrc[2]"\
-			             ${Subject32} "$GroupDirectory/${Subject32}.results/stats.${Subject32}_REML+tlrc[1]" "$GroupDirectory/${Subject32}.results/stats.${Subject32}_REML+tlrc[2]"\
-			             ${Subject33} "$GroupDirectory/${Subject33}.results/stats.${Subject33}_REML+tlrc[1]" "$GroupDirectory/${Subject33}.results/stats.${Subject33}_REML+tlrc[2]"\
-			             ${Subject34} "$GroupDirectory/${Subject34}.results/stats.${Subject34}_REML+tlrc[1]" "$GroupDirectory/${Subject34}.results/stats.${Subject34}_REML+tlrc[2]"\
-			             ${Subject35} "$GroupDirectory/${Subject35}.results/stats.${Subject35}_REML+tlrc[1]" "$GroupDirectory/${Subject35}.results/stats.${Subject35}_REML+tlrc[2]"\
-			             ${Subject36} "$GroupDirectory/${Subject36}.results/stats.${Subject36}_REML+tlrc[1]" "$GroupDirectory/${Subject36}.results/stats.${Subject36}_REML+tlrc[2]"\
-			             ${Subject37} "$GroupDirectory/${Subject37}.results/stats.${Subject37}_REML+tlrc[1]" "$GroupDirectory/${Subject37}.results/stats.${Subject37}_REML+tlrc[2]"\
-			             ${Subject38} "$GroupDirectory/${Subject38}.results/stats.${Subject38}_REML+tlrc[1]" "$GroupDirectory/${Subject38}.results/stats.${Subject38}_REML+tlrc[2]"\
-			             ${Subject39} "$GroupDirectory/${Subject39}.results/stats.${Subject39}_REML+tlrc[1]" "$GroupDirectory/${Subject39}.results/stats.${Subject39}_REML+tlrc[2]"\
-			             ${Subject40} "$GroupDirectory/${Subject40}.results/stats.${Subject40}_REML+tlrc[1]" "$GroupDirectory/${Subject40}.results/stats.${Subject40}_REML+tlrc[2]" 
+			    	     ${Subject19} "$GroupDirectory/${Subject19}.results/stats.${Subject19}_REML+tlrc[1]" "$GroupDirectory/${Subject19}.results/stats.${Subject19}_REML+tlrc[2]"\
+			             ${Subject20} "$GroupDirectory/${Subject20}.results/stats.${Subject20}_REML+tlrc[1]" "$GroupDirectory/${Subject20}.results/stats.${Subject20}_REML+tlrc[2]"
 
 				# Check if group results were created correctly
 				if [ -e $ResultsDirectory/${Smoothing}_${Design}_${Comparison}+tlrc.HEAD  ]; then
@@ -241,7 +180,7 @@ do
 					i=0
 
 					# Read all smoothness estimations from file
-					for subject in $(seq 0 $(($thirtynine)) )	
+					for subject in $(seq 0 $(($nineteen)) )	
 					do
 						smoothnesses=`cat $GroupDirectory/${Subjects[$((subject))]}.results/blur.errts.1D`
 						smoothnessstring=${smoothnesses[$(($i))]}
@@ -253,7 +192,7 @@ do
 					# Calculate mean x smoothness
 					XSmoothness=0.0
 					index=0
-					for subject in $(seq 0 $(($thirtynine)) )
+					for subject in $(seq 0 $(($nineteen)) )
 					do
 						XSmoothness=$(echo $XSmoothness + ${AllSmoothnesses[$(($index))]} | bc)
 						index=$((index + three))
@@ -263,7 +202,7 @@ do
 					# Calculate mean y smoothness
 					YSmoothness=0.0
 					index=1
-					for subject in $(seq 0 $(($thirtynine)) )
+					for subject in $(seq 0 $(($nineteen)) )
 					do
 						YSmoothness=$(echo $YSmoothness + ${AllSmoothnesses[$(($index))]} | bc)
 						index=$((index + three))
@@ -273,7 +212,7 @@ do
 					# Calculate mean z smoothness
 					ZSmoothness=0.0
 					index=2
-					for subject in $(seq 0 $(($thirtynine)) )
+					for subject in $(seq 0 $(($nineteen)) )
 					do
 						ZSmoothness=$(echo $ZSmoothness + ${AllSmoothnesses[$(($index))]} | bc)
 						index=$((index + three))
@@ -314,7 +253,7 @@ do
 
 					echo -e "\n"
 
-				        String=CLUSTERS #Search for NO CLUSTERS
+				    String=CLUSTERS #Search for NO CLUSTERS
 					File=clustersizes.txt
 					if grep -q $String "$File"; then 
 						echo "No significant group difference detected"
@@ -350,7 +289,7 @@ do
 
 					echo -e "\n"
 	
-				        String=CLUSTERS #Search for NO CLUSTERS
+				    String=CLUSTERS #Search for NO CLUSTERS
 					File=clustersizes.txt
 					if grep -q $String "$File"; then 
 						echo "No significant group difference detected"
@@ -377,13 +316,13 @@ do
 	
 		done
 
-		echo "Current FWE 1 is $FWE1" > Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize20.txt
-		echo "Number of failed group masks is $NoGroupMask" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize20.txt
-		echo "Number of failed group analyses is $NoGroupAnalysis" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize20.txt
+		echo "Current FWE 1 is $FWE1" > Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize10.txt
+		echo "Number of failed group masks is $NoGroupMask" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize10.txt
+		echo "Number of failed group analyses is $NoGroupAnalysis" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT1}_groupsize10.txt
 
-		echo "Current FWE 2 is $FWE2" > Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize20.txt
-		echo "Number of failed group masks is $NoGroupMask" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize20.txt
-		echo "Number of failed group analyses is $NoGroupAnalysis" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize20.txt
+		echo "Current FWE 2 is $FWE2" > Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize10.txt
+		echo "Number of failed group masks is $NoGroupMask" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize10.txt
+		echo "Number of failed group analyses is $NoGroupAnalysis" >> Results/results_AFNI_twosamplettest_${Study}_${Smoothing}_${DesignName}_MEMA_${CDT2}_groupsize10.txt
 
 	done	
 done
