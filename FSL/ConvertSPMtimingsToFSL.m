c3df49524f69be842afe2c0bc4a04a0882de5d26
Anders Eklund <andek67@harley.imt.liu.se>
2017-09-14 20:37:46 +0200

    Added HCP events

--- FSL/ConvertSPMtimingsToFSL.m
+++ FSL/ConvertSPMtimingsToFSL.m
@@ -8,10 +8,13 @@ clc
 %study = 'Cambridge';
 %load ../SPM/randomDifferenceEventsCambridgeAllSubjects.mat
 
-study = 'Oulu';
-load ../SPM/randomDifferenceEventsOuluAllSubjects.mat
+%study = 'Oulu';
+%load ../SPM/randomDifferenceEventsOuluAllSubjects.mat
 
-numEvents = 13;
+study = 'HCP';
+load ../SPM/randomDifferenceEventsHCPAllSubjects.mat
+
+numEvents = 26;
 
 BeijingSubjects={
     
@@ -527,20 +530,128 @@ OuluSubjects = {
 
 };
 
-for subject = 1:103
+HCPSubjects = {
+    
+
+'100307'
+'100408'
+'101107'
+'101309'
+'101915'
+'103111'
+'103414'
+'103818'
+'105014'
+'105115'
+'106016'
+'108828'
+'110411'
+'111312'
+'111716'
+'113619'
+'113922'
+'114419'
+'115320'
+'116524'
+'117122'
+'118528'
+'118730'
+'118932'
+'120111'
+'122317'
+'122620'
+'123117'
+'123925'
+'124422'
+'125525'
+'126325'
+'127630'
+'127933'
+'128127'
+'128632'
+'129028'
+'130013'
+'130316'
+'131217'
+'131722'
+'133019'
+'133928'
+'135225'
+'135932'
+'136833'
+'138534'
+'139637'
+'140925'
+'144832'
+'146432'
+'147737'
+'148335'
+'148840'
+'149337'
+'149539'
+'149741'
+'151223'
+'151526'
+'151627'
+'153025'
+'154734'
+'156637'
+'159340'
+'160123'
+'161731'
+'162733'
+'163129'
+'176542'
+'178950'
+'188347'
+'189450'
+'190031'
+'192540'
+'196750'
+'198451'
+'199655'
+'201111'
+'208226'
+'211417'
+'211720'
+'212318'
+'214423'
+'221319'
+'239944'
+'245333'
+'280739'
+'298051'
+'366446'
+'397760'
+'414229'
+'499566'
+'654754'
+'672756'
+'751348'
+'756055'
+'792564'
+'856766'
+'857263'
+'899885'
+
+
+};
+
+for subject = 1:100
     
     %subjectString = BeijingSubjects{subject};
     %subjectString = CambridgeSubjects{subject};
-    subjectString = OuluSubjects{subject};
+    %subjectString = OuluSubjects{subject};
+    subjectString = HCPSubjects{subject};
     
-    fid = fopen(['/home/andek/Research_projects/RandomGroupAnalyses/' study '_randomevents/differenceEventsstim1_' subjectString '.txt'],'w');    
+    fid = fopen(['/home/andek67/Research_projects/RandomGroupAnalyses/' study '_randomevents/differenceEventsstim1_' subjectString '.txt'],'w');    
     for event = 1:numEvents
         fprintf(fid, ' %i  %i  1 \n ', allRandomOnsets1(subject,event), allRandomDurations1(subject,event));
     end
     fprintf(fid,' \n ');        
     fclose(fid);
     
-    fid = fopen(['/home/andek/Research_projects/RandomGroupAnalyses/' study '_randomevents/differenceEventsstim2_' subjectString '.txt'],'w');    
+    fid = fopen(['/home/andek67/Research_projects/RandomGroupAnalyses/' study '_randomevents/differenceEventsstim2_' subjectString '.txt'],'w');    
     for event = 1:numEvents
         fprintf(fid, ' %i  %i  1 \n', allRandomOnsets2(subject,event), allRandomDurations2(subject,event));
     end
